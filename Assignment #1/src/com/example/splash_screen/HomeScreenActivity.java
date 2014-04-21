ea9aa95e976a5b462a9dd3950d615fe8d305cedd
annnik <nikishina.a.l@gmail.com>
2014-04-21 15:17:57 +0400

    land&portrait

--- Assignment #1/src/com/example/splash_screen/HomeScreenActivity.java
+++ Assignment #1/src/com/example/splash_screen/HomeScreenActivity.java
@@ -1,39 +1,41 @@
 package com.example.splash_screen;
 
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class HomeScreenActivity extends Activity {
 
-	
-	
-	 String[] names = new String[20];
-	 
-	 void InputArray() 
-	 { 
-		 for (int i = 0; i < 20; i++) 
-		 { 
-			 names[i] = "Element #"
-	  + String.valueOf(i+1); 
-			 } 
-		 }
-	 
+	String[] names = new String[20];
+
+	void InputArray() {
+		for (int i = 0; i < 20; i++) {
+			names[i] = "Element #" + String.valueOf(i + 1);
+		}
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		 InputArray();
+		InputArray();
 		// TODO com.example.ClearScreen
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.a_main);
-
+		
 		ListView ListOfElements = (ListView) findViewById(R.id.ListOfElements);
 
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
 				android.R.layout.simple_list_item_1, names);
 
 		ListOfElements.setAdapter(adapter);
+
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		setContentView(R.layout.a_splash);
 
 	}
 }
--- Assignment #1/src/com/example/splash_screen/MainActivity.java
+++ Assignment #1/src/com/example/splash_screen/MainActivity.java
@@ -1,45 +1,49 @@
 package com.example.splash_screen;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.Fragment;
+//import android.support.v7.app.ActionBarActivity;
+//import android.support.v4.app.Fragment;
 import android.os.Bundle;
+//import android.content.res.Configuration;
 import android.os.Handler;
+import android.app.Activity;
 import android.content.Intent;
 
-public class MainActivity extends ActionBarActivity {
-
+public class MainActivity extends Activity {
+//ActionBarActivity
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.a_splash);
-		
-			
-			new Handler().postDelayed(new Runnable() {
-	            @Override
-	            public void run() {
-	            	startActivity(new Intent(
-							"com.example.splash_screen.HomeScreenActivity"));
-	            	finish();	           
-	            }
-	        }, 2000);
-			
-	
-		if (savedInstanceState == null) {
+
+		new Handler().postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				startActivity(new Intent(
+						"com.example.splash_screen.HomeScreenActivity"));
+				// finish();
+			}
+		}, 2000);
+
+		/*if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
+		}*/
+
 	}
-
-
 
 	/**
 	 * A placeholder fragment containing a simple view.
 	 */
-	public static class PlaceholderFragment extends Fragment {
+	/*public static class PlaceholderFragment extends Fragment {
 
 		public PlaceholderFragment() {
 		}
 
 	}
+	@Override
+    public void onConfigurationChanged(Configuration newConfig) {
+    super.onConfigurationChanged(newConfig);
+    setContentView(R.layout.a_splash);
+    }*/
 
 }
