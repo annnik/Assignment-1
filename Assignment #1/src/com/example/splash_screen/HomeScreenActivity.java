3019864a67b705422770b4824d44a19e2dbc68b6
annnik <nikishina.a.l@gmail.com>
2014-04-21 21:17:40 +0400

    Возникновение сплеша при отловленном повороте - в комментариях.

--- Assignment #1/src/com/example/splash_screen/HomeScreenActivity.java
+++ Assignment #1/src/com/example/splash_screen/HomeScreenActivity.java
@@ -1,7 +1,7 @@
 package com.example.splash_screen;
 
 import android.app.Activity;
-import android.content.res.Configuration;
+//import android.content.res.Configuration;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -9,18 +9,31 @@ import android.widget.ListView;
 public class HomeScreenActivity extends Activity {
 
 	String[] names = new String[20];
-
+	
 	void InputArray() {
 		for (int i = 0; i < 20; i++) {
 			names[i] = "Element #" + String.valueOf(i + 1);
 		}
 	}
+	
+	/*@Override
+	public void onConfigurationChanged(Configuration newConfig) {
 
+		super.onConfigurationChanged(newConfig);
+		if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+			setContentView(R.layout.a_splash);
+	    } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+	    	setContentView(R.layout.a_splash);
+	    }
+		
+	}
+	*/
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		InputArray();
 		// TODO com.example.ClearScreen
 		super.onCreate(savedInstanceState);
+		
 		setContentView(R.layout.a_main);
 		
 		ListView ListOfElements = (ListView) findViewById(R.id.ListOfElements);
@@ -29,13 +42,8 @@ public class HomeScreenActivity extends Activity {
 				android.R.layout.simple_list_item_1, names);
 
 		ListOfElements.setAdapter(adapter);
-
+		
 	}
 
-	@Override
-	public void onConfigurationChanged(Configuration newConfig) {
-		super.onConfigurationChanged(newConfig);
-		setContentView(R.layout.a_splash);
-
-	}
+	
 }
--- Assignment #1/src/com/example/splash_screen/MainActivity.java
+++ Assignment #1/src/com/example/splash_screen/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.splash_screen;
 
 //import android.support.v7.app.ActionBarActivity;
-//import android.support.v4.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
 //import android.content.res.Configuration;
 import android.os.Handler;
@@ -9,41 +9,55 @@ import android.app.Activity;
 import android.content.Intent;
 
 public class MainActivity extends Activity {
-//ActionBarActivity
+	
+/*	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+
+		super.onConfigurationChanged(newConfig);
+		if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+			setContentView(R.layout.a_main);
+	    } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+	    	setContentView(R.layout.a_main);
+	    }
+		
+	}
+	private void getScreenOrientation(){    
+		if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT)
+			setContentView(R.layout.a_main);
+		else if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE)
+			setContentView(R.layout.a_main);
+		
+	}*/
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		//getScreenOrientation();
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.a_splash);
+		
 
 		new Handler().postDelayed(new Runnable() {
 			@Override
 			public void run() {
 				startActivity(new Intent(
 						"com.example.splash_screen.HomeScreenActivity"));
-				// finish();
+				
 			}
 		}, 2000);
 
-		/*if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}*/
+		
 
 	}
 
 	/**
 	 * A placeholder fragment containing a simple view.
 	 */
-	/*public static class PlaceholderFragment extends Fragment {
+
+	public static class PlaceholderFragment extends Fragment {
 
 		public PlaceholderFragment() {
 		}
 
 	}
-	@Override
-    public void onConfigurationChanged(Configuration newConfig) {
-    super.onConfigurationChanged(newConfig);
-    setContentView(R.layout.a_splash);
-    }*/
 
+	
 }
