9082e3b59fe849addd446d7116f01ffef488fd5e
annnik <nikishina.a.l@gmail.com>
2014-04-23 18:55:07 +0400

    Fixed bug with orientation change

--- SplashScreen/src/com/assignment1/splashscreen/HomeScreenActivity.java
+++ SplashScreen/src/com/assignment1/splashscreen/HomeScreenActivity.java
@@ -1,7 +1,7 @@
 package com.assignment1.splashscreen;
 
-
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -9,37 +9,39 @@ import android.widget.ListView;
 public class HomeScreenActivity extends Activity {
 
 	private String[] names = new String[20];
-	
+
 	private void initNamesArray() {
 		for (int i = 0; i < names.length; i++) {
 			names[i] = "Element #" + String.valueOf(i + 1);
 		}
 	}
-	
-	
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		initNamesArray();
-		
+
 		super.onCreate(savedInstanceState);
-		
+
 		setContentView(R.layout.a_homescreen);
-		
+
 		ListView ListOfNames = (ListView) findViewById(R.id.listofelementsnames);
 
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
 				android.R.layout.simple_list_item_1, names);
 
 		ListOfNames.setAdapter(adapter);
-		
-	}
-	@Override
-	protected void onPause() {
-		 super.onPause();
-		HomeScreenActivity.this.finish();
-		
+
 	}
 
-	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		HomeScreenActivity.this.finish();
+
+	}
+	public void onConfigurationChanged(Configuration newConfig) {
+		 super.onConfigurationChanged(newConfig);
+		 HomeScreenActivity.this.finish();
+		 }
+
 }
--- SplashScreen/src/com/assignment1/splashscreen/SplashScreenActivity.java
+++ SplashScreen/src/com/assignment1/splashscreen/SplashScreenActivity.java
@@ -6,28 +6,26 @@ import android.app.Activity;
 import android.content.Intent;
 
 public class SplashScreenActivity extends Activity {
-	
-	private static final int timeOfWaiting=2000;
+
+	private static final int timeOfWaiting = 2000;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.a_splashscreen);
-		
- 
+
 		final Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            public void run() {
-               
-                Intent mInHome = new Intent(SplashScreenActivity.this, HomeScreenActivity.class);
-                SplashScreenActivity.this.startActivity(mInHome);
-                
-            }
-        }, timeOfWaiting);
+		handler.postDelayed(new Runnable() {
+			public void run() {
+
+				Intent mInHome = new Intent(SplashScreenActivity.this,
+						HomeScreenActivity.class);
+				SplashScreenActivity.this.startActivity(mInHome);
+
+			}
+		}, timeOfWaiting);
 
 	}
 
-	
-
-	
 }
