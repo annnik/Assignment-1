5d39f10c3af137638e16b870f7bf8e41a1be57ab
annnik <nikishina.a.l@gmail.com>
2014-05-08 10:40:03 +0400

    changing condition

--- SplashScreen/src/com/assignment1/splashscreen/SplashScreenActivity.java
+++ SplashScreen/src/com/assignment1/splashscreen/SplashScreenActivity.java
@@ -13,7 +13,7 @@ public class SplashScreenActivity extends Activity {
 	private long activityStartingTimeMilliseconds=0;
 	private long appStartingTimeMilliseconds;
 	private final Handler handler = new Handler();
-	private static final int timeOfWaiting = 2000;
+	private static final int timeOfWaiting = 10000;
 	private static final String TIME_FROM_START = "timeFromStart";
 	private Runnable runnableActivityStart = new Runnable() {
 		public void run() {
@@ -27,7 +27,7 @@ public class SplashScreenActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 
-		if (appStartingTimeMilliseconds == 0) {
+		if (savedInstanceState == null){
 			appStartingTimeMilliseconds = (new Date()).getTime();
 		}
 		if (savedInstanceState != null) {
